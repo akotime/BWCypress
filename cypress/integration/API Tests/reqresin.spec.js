5029b6272d91d8e1387077d369b60204c8d36ced
AdamK <adam_zabrze@poczta.fm>
2021-07-14 23:01:11 +0200

    added tests for POST, PUT, PATCH, DELETE methods

--- cypress/integration/API Tests/reqresin.spec.js
+++ cypress/integration/API Tests/reqresin.spec.js
@@ -1,10 +1,17 @@
 /// <reference types="cypress" />
 
-describe('REST API Test for reqres.in with Cypress', () => {
-    it('Create User (Post) - Validate Header content-type', () => {
+describe('REST API Test create user in reqres.in with Cypress', () => {
+    beforeEach('Go to the reqres.in website', () => {
+        cy.fixture('reqres.json').then(reqres => {
+            const baseURL = reqres.baseUrl
+            cy.visit(baseURL)
+        })
+    })
+
+    it('Validate creating new user - POST', () => {
         cy.request({
             method: 'POST',
-            url: 'https://reqres.in/api/users',
+            url: '/api/users',
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -12,55 +19,312 @@ describe('REST API Test for reqres.in with Cypress', () => {
                 "name": "tester",
                 "job": "qa"
             }
-        }).as('createUser').then((request) => {
-            cy.log(request.body)
+        }).as('createUser').then((response) => {
+            cy.log(response.body)
+            //cy.writeFile('cypress/fixtures/user.json', response.body)
+        })
+        
+        cy.get('@createUser')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+
+        cy.get('@createUser') 
+            .its('status')
+            .should('equal', 201)
+
+        cy.get('@createUser')
+            .its('body')
+            .should('include', {
+                name: 'tester',
+                 job: 'qa'
+            })
+    })
+
+    it('Validate creating empty user - POST', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/users',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).as('createUser').then((response) => {
+            cy.log(response.body)
+        })
+
+        cy.get('@createUser') 
+            .its('status')
+            .should('equal', 201)
+
+        cy.get('@createUser')
+            .its('body')
+            .should('not.include', {
+                name: 'tester',
+                 job: 'qa'
+            })
+    })
+
+    it('Validate negative example of creating user for non existing endpoint - POST', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/',
+            failOnStatusCode: false
+        }).as('createUser').then((response) => {
+            cy.log(response.body)
+        })
+
+        cy.get('@createUser')
+            .its('status')
+            .should('equal', 404)
+    })
+
+    it('Validate Update User - PUT', () => {
+        cy.request({
+            method: 'PUT',
+            url: '/api/users/2',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                "name": "Adam",
+                "job": "Dev"
+            }
+        }).as('createUser').then((response) => {
+            cy.log(response.body)
+            //cy.writeFile('cypress/fixtures/userUpdated.json', response.body)
         })
 
         cy.get('@createUser')
             .its('headers')
             .its('content-type')
             .should('include', 'application/json; charset=utf-8')
-    })
-
-    it('Create User (Post) - Validate Response Status Code', () => {
-        cy.request({
-            method: 'POST',
-            url: 'https://reqres.in/api/users',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: {
-                "name": "tester",
-                "job": "qa"
-            }
-        }).as('createUser').then((request) => {
-            cy.log(request.body)
-        })
 
         cy.get('@createUser') 
             .its('status')
-            .should('equal', 201)
-    })
-
-    it('Create User (Post) - Validate Response Body Elements', () => {
-        cy.request({
-            method: 'POST',
-            url: 'https://reqres.in/api/users',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: {
-                "name": "tester",
-                "job": "qa"
-            }
-        }).as('createUser').then((request) => {
-            cy.log(request.body)
-        })
+            .should('equal', 200)
 
         cy.get('@createUser')
             .its('body')
             .should('include', {
-                name: 'tester',
-                 job: 'qa'})
+                name: 'Adam',
+                 job: 'Dev'
+            })
+
+        cy.get('@createUser')
+            .its('body')
+            .should('have.keys', 'name', 'job', 'updatedAt')
+    })
+
+    it('Validate Update User - PATCH', () => {
+        cy.request({
+            method: 'PATCH',
+            url: '/api/users/2',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                "name": "John",
+                "job": "Engineer"
+            }
+        }).as('createUser').then((response) => {
+            cy.log(response.body)
+            //cy.writeFile('cypress/fixtures/userUpdatedPatch.json', response.body)
+        })
+
+        cy.get('@createUser')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+
+        cy.get('@createUser') 
+            .its('status')
+            .should('equal', 200)
+
+        cy.get('@createUser')
+            .its('body')
+            .should('include', {
+                name: 'John',
+                 job: 'Engineer'
+            })
+
+        cy.get('@createUser')
+            .its('body')
+            .should('have.keys', 'name', 'job', 'updatedAt')
+    })
+
+    it('Validate Deleting User - DELETE', () => {
+        cy.request({
+            method: 'DELETE',
+            url: '/api/users/2',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).as('createUser').then((response) => {
+            cy.log(response.body)
+        })
+
+        cy.get('@createUser')
+            .its('headers')
+            .its('content-length')
+            .should('equal', '0')
+
+        cy.get('@createUser') 
+            .its('status')
+            .should('equal', 204)
+    })
+
+    it('Validate Registration of known user (Register Successful) - POST ', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/register',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                "email": "eve.holt@reqres.in",
+                "password": "pistol"
+            }
+        }).as('register').then((response) => {
+            cy.log(response.body)
+        })
+
+        cy.get('@register')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+
+        cy.get('@register') 
+            .its('status')
+            .should('equal', 200)
+
+        cy.get('@register')
+            .its('body')
+            .should('include', {id: 4, token: "QpwL5tke4Pnpja7X4"})
+    })
+
+    it('Validate Registration of unknown user (Register Unsuccessful)- POST ', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/register',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                "email": "unknow.user@reqres.in",
+                "password": "pistol"
+            },
+            failOnStatusCode: false
+        }).as('register').then((response) => {
+            cy.log(response.body)
+        })
+
+        cy.get('@register')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+
+        cy.get('@register') 
+            .its('status')
+            .should('equal', 400)
+
+        cy.get('@register')
+            .its('body')
+            .should('include', {
+                "error": "Note: Only defined users succeed registration"
+            })
+    })
+
+    it('Validate Login - Successful - POST ', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/login',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                "email": "eve.holt@reqres.in",
+                "password": "cityslicka"
+            }
+        }).as('register').then((response) => {
+            cy.log(response.body)
+        })
+
+        cy.get('@register')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+
+        cy.get('@register') 
+            .its('status')
+            .should('equal', 200)
+
+        cy.get('@register')
+            .its('body')
+            .should('include', {
+                "token": "QpwL5tke4Pnpja7X4"
+            })
+    })
+
+    it('Validate Login Unsuccessful (Missing Password) - POST ', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/login',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                "email": "peter@klaven"
+            },
+            failOnStatusCode: false
+        }).as('register').then((response) => {
+            cy.log(response.body)
+        })
+
+        cy.get('@register')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+
+        cy.get('@register') 
+            .its('status')
+            .should('equal', 400)
+
+        cy.get('@register')
+            .its('body')
+            .should('include', {
+                "error": "Missing password"
+            })
+    })
+
+    it('Validate Login Unsuccessful (User Not Found) - POST ', () => {
+        cy.request({
+            method: 'POST',
+            url: '/api/login',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                "email": "test@test.com",
+                "password": "password"
+            },
+            failOnStatusCode: false
+        }).as('register').then((response) => {
+            cy.log(response.body)
+        })
+
+        cy.get('@register')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json; charset=utf-8')
+
+        cy.get('@register') 
+            .its('status')
+            .should('equal', 400)
+
+        cy.get('@register')
+            .its('body')
+            .should('include', { 
+                error: 'user not found'
+            })
     })
 })
