b271b30ffa004d85fa1cb61b0858c6fefb2cde67
AdamK <adam_zabrze@poczta.fm>
2021-07-18 14:28:54 +0200

    fixture signup was added

--- cypress/integration/UI Tests/register.spec.js
+++ cypress/integration/UI Tests/register.spec.js
@@ -5,96 +5,109 @@ import HomeAddressBookPage from '../../support/PageObjects/HomeAddressBookPage'
 import SignUpPage from '../../support/PageObjects/SignUpPage'
 
 describe('Positive Sign Up Tests', () => {
-    beforeEach('', () => {
-        cy.visit('http://a.testaddressbook.com/sign_up')
+
+    beforeEach('Load Sign Up Page', () => {
+
+        cy.fixture('signUpData.json').then((signUpData) => {
+            cy.visit(signUpData.signUpURL)
+        })
     })
 
     it('Sign Up a new user and logout them', () => {
 
         const homeAddressBook = new HomeAddressBookPage()
 
-        // Generate random email address and sign up 
-        cy.genSimpleRandomEmail('test.com').then((randomEmail) => {
-            cy.signUpUser(randomEmail, 'Password_12345')
-            
-            // Validate URL addres was changed
-            cy.url().should('include', 'http://a.testaddressbook.com/')
+        cy.fixture('signUpData.json').then((signUpData) => {
+            // Generate random email address and sign up 
+            cy.genSimpleRandomEmail(signUpData.domainName).then((randomEmail) => {
+                cy.signUpUser(randomEmail, signUpData.password)
+                
+                // Validate URL addres was changed
+                cy.url().should('include', signUpData.baseURL)
 
-            // Validate if there is a username email in navbar
-            homeAddressBook.getLogInUsername().should('have.text', randomEmail.toLowerCase())
+                // Validate if there is a username email in navbar
+                homeAddressBook.getLogInUsername().should('have.text', randomEmail.toLowerCase())
 
-            // Sign Out 
-            homeAddressBook.getSignOutLink().click()
+                // Sign Out 
+                homeAddressBook.getSignOutLink().click()
 
-            // Validate it you are moved to the Login Page
-            cy.url().should('include', 'http://a.testaddressbook.com/sign_in')
+                // Validate if you are moved to the Login Page
+                cy.url().should('include', signUpData.signInURL)
+            })
         })
     })
         
-
     it('Sign Up a new user, go to the new address page and logout them', () => {
 
         const homeAddressBook = new HomeAddressBookPage()
 
-        // Generate random email address and sign up 
-        cy.genSimpleRandomEmail('test.com').then((randomEmail) => {
-            cy.signUpUser(randomEmail, 'Password_12345')
-            
-            // Validate URL addres was changed
-            cy.url().should('include', 'http://a.testaddressbook.com/')
+        cy.fixture('signUpData.json').then((signUpData) => {
+            // Generate random email address and sign up 
+            cy.genSimpleRandomEmail(signUpData.domainName).then((randomEmail) => {
+                cy.signUpUser(randomEmail, signUpData.password)
+                
+                // Validate URL addres was changed
+                cy.url().should('include', signUpData.baseURL)
 
-            // Validate if there is a username email in navbar
-            homeAddressBook.getLogInUsername().should('have.text', randomEmail.toLowerCase())
-            
-            // Move to the Address Page
-            homeAddressBook.getAddressesLink().click()
+                // Validate if there is a username email in navbar
+                homeAddressBook.getLogInUsername().should('have.text', randomEmail.toLowerCase())
+                
+                // Move to the Address Page
+                homeAddressBook.getAddressesLink().click()
 
-            const address = new AddressPage()
-            // Validate you are in the Address page
-            address.getAddressText().should('have.text', 'Addresses')
+                const address = new AddressPage()
+                // Validate you are in the Address page
+                address.getAddressText().should('have.text', signUpData.addressText)
 
-            // Click Add New Address link and validate you are in the adding address form page
-            address.getNewAddressesLink().click()
-            cy.url().should('include', 'addresses/new')
+                // Click Add New Address link and validate you are in the adding address form page
+                address.getNewAddressesLink().click()
+                cy.url().should('include', signUpData.newAddresse)
 
-            // Sign Out 
-            homeAddressBook.getSignOutLink().click()
+                // Sign Out 
+                homeAddressBook.getSignOutLink().click()
 
-            // Validate it you are moved to the Login Page
-            cy.url().should('include', 'http://a.testaddressbook.com/sign_in')
-        })               
+                // Validate it you are moved to the Login Page
+                cy.url().should('include', signUpData.signInURL)
+            })  
+        })             
     })
 
 })
 
 describe('Negative Signe Up Tests', () => {
     beforeEach('', () => {
-        cy.visit('http://a.testaddressbook.com/sign_up')
+        cy.fixture('signUpData.json').then((signUpData) => {
+            cy.visit(signUpData.signUpURL)
+        })    
     })
 
     it('Validate if you stay at the Sign Up Page after sign up existing user', () => {
 
-        // Sign Up existing user
-        cy.signUpUser('testak@test.com', 'Password_12345')
+        cy.fixture('signUpData.json').then((signUpData) => {
+            // Sign Up existing user
+            cy.signUpUser(signUpData.username, signUpData.password)
 
-        // Validate the endpoint is users
-        cy.url().should('include', 'users')
+            // Validate the endpoint is users
+            cy.url().should('include', signUpData.endpointUsers)
 
-        // Validate the header of the site is still Sign Up 
-        const signUpPage = new SignUpPage()
-        signUpPage.getSignUpTitle().should('include.text', 'Sign up')
+            // Validate the header of the site is still Sign Up 
+            const signUpPage = new SignUpPage()
+            signUpPage.getSignUpTitle().should('include.text', signUpData.signUpPageTitle)
+        })
     })
 
     it('Click Sign Up button without email and password', () => {
 
-        // Sign Up existing user
-        cy.signUpUser('', '')
+        cy.fixture('signUpData.json').then((signUpData) => {
+            // Sign Up existing user
+            cy.signUpUser(signUpData.emptyUsername, signUpData.emptyPassword)
 
-        // Validate the endpoint is users
-        cy.url().should('include', 'users')
+            // Validate the endpoint is users
+            cy.url().should('include', signUpData.endpointUsers)
 
-        // Validate the header of the site is still Sign Up 
-        const signUpPage = new SignUpPage()
-        signUpPage.getSignUpTitle().should('include.text', 'Sign up')
+            // Validate the header of the site is still Sign Up 
+            const signUpPage = new SignUpPage()
+            signUpPage.getSignUpTitle().should('include.text', signUpData.signUpPageTitle)
+        })
     })
 })
